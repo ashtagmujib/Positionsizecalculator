092edacc33c39ce556f8e7c791ffc0b5b4e37f98
Abdul-Mujib <mujibajimoh@gmail.com>
2026-01-24 20:27:39 +0100

    code adjustment

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
+const body = document.querySelector('body');
+const sun = document.getElementById('sun');
+const moon = document.getElementById('moon');
+
+const BTN = document.getElementById('btn');
+const result = document.getElementById('result');
+
+
+let theme;
+
+// dark and light mode
+moon.addEventListener('click', darkmode);
+sun.addEventListener('click', lightmode);
+
+
+function darkmode() {
+    theme = 'dark';
+
+    moon.style.display = 'none';
+    sun.style.display = 'block';
+
+    // change colors
+    body.style.backgroundColor = '#04090F';
+    body.style.color = '#D9E3E5';
+    
+    BTN.style.backgroundColor = 'white';
+    BTN.style.color = 'black';
+
+    result.style.backgroundColor = 'white';
+    result.style.color = 'black';
+
+
+    localStorage.setItem('theme', JSON.stringify(theme))
+
+}
+
+
+
+function lightmode() {
+    theme = 'light';
+
+    moon.style.display = 'block';
+    sun.style.display = 'none';
+
+    // change colors
+    body.style.backgroundColor = '#fff';
+    body.style.color = '#04090F';
+
+    BTN.style.backgroundColor = 'black';
+    BTN.style.color = 'white';
+
+    result.style.backgroundColor = 'black';
+    result.style.color = 'white';
+    localStorage.setItem('theme', JSON.stringify(theme))
+}
+
+
+
+
+let parsedTheme = JSON.parse(localStorage.getItem('theme'))
+
+if(parsedTheme === 'dark' ) {
+    moon.style.display = 'none';
+    sun.style.display = 'block';
+
+    // change colors
+    body.style.backgroundColor = '#04090F';
+    body.style.color = '#D9E3E5';
+}
+
+else if (parsedTheme === 'light') {
+    moon.style.display = 'block';
+    sun.style.display = 'none';
+
+    // change colors
+    body.style.backgroundColor = '#fff';
+    body.style.color = '#04090F';
+}
+
+
 function calculate() {
     const risk = parseFloat(document.getElementById("risk").value);
     const entry = parseFloat(document.getElementById("entry").value);
@@ -6,13 +86,20 @@ function calculate() {
     if (!risk || !entry || !stop) {
       alert("Please fill in all fields");
       return;
-    }
+    }  
 
     const positionSize =
       (risk * entry) / Math.abs(entry - stop);
 
 
-    document.getElementById("result").innerHTML = `
-      <strong>Position Size:</strong> $${positionSize.toFixed(2)}<br>
-    `;
-}
+    RST= document.getElementById("result");
+    RST.style.display = 'block';
+    RST.style.backgroundColor = 'black';
+    RST.innerHTML = `<strong>Order Value:</strong> $${positionSize.toFixed(2)}<br>`;  
+}    
+
+
+
+
+
+
